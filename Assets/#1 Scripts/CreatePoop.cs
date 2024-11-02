f04eb0513e038f2416b1691ca3f6aefa40161855
이형주 <jujumom630@gmail.com>
2024-11-02 09:57:19 +0900

    채훈...아니 똥 피하기

---
+++ Assets/#1 Scripts/CreatePoop.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePoop : MonoBehaviour
+{
+    public GameObject Poop;
+    public float spawnInterval = 1.0f; // 장애물 생성 간격
+    public float spawnRangeX = 8.0f; // x축 범위 설정
+    private float timer = 0.0f; // 시간 누적을 위한 변수
+
+    void Update()
+    {
+        // 시간이 일정 간격만큼 지나면 장애물 생성
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval)
+        {
+            SpawnObstacle();
+            timer = 0.0f; // 타이머 초기화
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        // 장애물이 생성될 랜덤 x 위치 설정
+        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+        Vector3 spawnPosition = new Vector3(randomX, transform.position.y, 0);
+        
+        // 장애물 생성
+        Instantiate(Poop, spawnPosition, Quaternion.identity);
+    }
+}
