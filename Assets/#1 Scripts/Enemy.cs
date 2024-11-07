f9eef004907d532d7c93e536b8987b128fcd1182
이형주 <jujumom630@gmail.com>
2024-11-08 00:47:32 +0900

    enemy 사망

--- Assets/#1 Scripts/Enemy.cs
+++ Assets/#1 Scripts/Enemy.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float enemyHP;
+    public Sprite deadSprite;  // Assign this in the Inspector to the sprite for when the enemy dies
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyHP = 2;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemyHP <= 0)
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "bullet")
+        {
+            enemyHP -= 1;
+        }
+        else if(other.tag == "Player")
+        {
+            
+        }
+    }
+
+    IEnumerator Die()
+    {
+        gameObject.tag = "Dead";
+
+        spriteRenderer.sprite = deadSprite;
+
+        yield return new WaitForSeconds(0.75f);
+
+        Destroy(gameObject);
     }
 }
