5168d02132e914f0535130c32c8e22b71a8a9dad
DNSK <sslgh1024@naver.com>
2024-11-11 13:45:39 +0900

    동기적으로 작동되도록 고침

--- Assets/#1 Scripts/DB_Manager.cs
+++ Assets/#1 Scripts/DB_Manager.cs
@@ -1,134 +1,111 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
-
-
 [System.Serializable]
 public class GoogleData
 {
-	public string order, result, msg, value;
+    public string order, result, msg, value;
 }
 
 public class DB_Manager : MonoBehaviour
 {
-	const string URL = "https://script.google.com/macros/s/AKfycbyvmxjwFUBJ08r55aaTLg8JKZNvQxHGCDTh1gUoTlx-2X-sWRECmAMB0NIEdVmHFCH8/exec";
-	public GoogleData GD;
-	public InputField IDInput, PassInput, ValueInput;
-	string id, pass;
-	
-	
-	
-	
-	
-	bool SetIDPass()
-	{
-		id = IDInput.text.Trim();
-		pass = PassInput.text.Trim();
+    const string URL = "https://script.google.com/macros/s/AKfycbxO-1D34Uh8E0ZnuBpTSpEU3JJ8pCQQuYBxjSwBmMwxrZGFTmvQFu0JxQD23B7EUENJ/exec";
+    public GoogleData GD;
+    string id, pass;
 
-		if (id == "" || pass == "") return false;
-		else return true;
-	}
-	
-	public void Register()
-	{
-		if (!SetIDPass())
-		{
-			print("아이디 또는 비밀번호가 비어있습니다");
-			return;
-		}
-		WWWForm form = new WWWForm();
-		form.AddField("order", "register");
-		form.AddField("id", id);
-		form.AddField("pass", pass);
+    private void Start()
+    {
+        StartCoroutine(ProcessRanks());
+    }
 
-		StartCoroutine(Post(form));
-	}
-	
-	public void Login()
-	{
-		if (!SetIDPass())
-		{
-			print("아이디 또는 비밀번호가 비어있습니다");
-			return;
-		}
-		WWWForm form = new WWWForm();
-		form.AddField("order", "login");
-		form.AddField("id", id);
-		form.AddField("pass", pass);
+    IEnumerator ProcessRanks()
+    {
+        yield return StartCoroutine(SetRank(3, "70", "20718", "이건희"));
+        yield return StartCoroutine(SetRank(3, "80", "20715", "심상균"));
+        yield return StartCoroutine(SetRank(3, "90", "20706", "나현서"));
+    }
 
-		StartCoroutine(Post(form));
-	}
-	
-	
-	void OnApplicationQuit()
-	{
-		WWWForm form = new WWWForm();
-		form.AddField("order", "logout");
+    IEnumerator SetRank(int game, string data, string id, string pass)
+    {
+        // Register
+        yield return StartCoroutine(Register(id, pass));
 
-		StartCoroutine(Post(form));
-	}
+        // Login
+        yield return StartCoroutine(Login(id, pass));
 
+        // SetValue
+        string value = game + "_" + data;
+        yield return StartCoroutine(SetValue(value));
+    }
 
-	public void SetValue()
-	{
-		WWWForm form = new WWWForm();
-		form.AddField("order", "setValue");
-		form.AddField("value", ValueInput.text);
+    IEnumerator Register(string id, string pass)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("order", "register");
+        form.AddField("id", id);
+        form.AddField("pass", pass);
+        yield return StartCoroutine(Post(form));
+    }
 
-		StartCoroutine(Post(form));
-	}
+    IEnumerator Login(string id, string pass)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("order", "login");
+        form.AddField("id", id);
+        form.AddField("pass", pass);
+        yield return StartCoroutine(Post(form));
+    }
 
+    IEnumerator SetValue(string value)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("order", "setValue");
+        form.AddField("value", value);
+        yield return StartCoroutine(Post(form));
+    }
 
-	public void GetValue()
-	{
-		string input = ValueInput.text.Trim();
+    IEnumerator GetValue(int gameNumber)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("order", "getValue");
+        form.AddField("value", gameNumber.ToString());
+        yield return StartCoroutine(Post(form));
+    }
 
-		WWWForm form = new WWWForm();
-		form.AddField("order", "getValue");
-		form.AddField("value", int.Parse(input));
+    IEnumerator Post(WWWForm form)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
+        {
+            yield return www.SendWebRequest();
 
-		StartCoroutine(Post(form));
-	}
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + www.error);
+            }
+            else
+            {
+                Response(www.downloadHandler.text);
+            }
+        }
+    }
+    void Response(string json)
+    {
+        if (string.IsNullOrEmpty(json)) return;
+        GD = JsonUtility.FromJson<GoogleData>(json);
 
+        if (GD.result == "ERROR")
+        {
+            Debug.Log(GD.order + "을 실행할 수 없습니다. 에러 메시지 : " + GD.msg);
+            return;
+        }
 
+        Debug.Log(GD.order + "을 실행했습니다. 메시지 : " + GD.msg);
 
-
-
-	IEnumerator Post(WWWForm form)
-	{
-		using (UnityWebRequest www = UnityWebRequest.Post(URL, form)) // 반드시 using을 써야한다
-		{
-			yield return www.SendWebRequest();
-
-			if (www.isDone) Response(www.downloadHandler.text);
-			else print("웹의 응답이 없습니다.");
-			
-			www.Dispose();
-		}
-		
-	}
-
-
-	void Response(string json)
-	{
-		if (string.IsNullOrEmpty(json)) return;
-
-		GD = JsonUtility.FromJson<GoogleData>(json);
-
-		if (GD.result == "ERROR")
-		{
-			print(GD.order + "을 실행할 수 없습니다. 에러 메시지 : " + GD.msg);
-			return;
-		}
-
-		print(GD.order + "을 실행했습니다. 메시지 : " + GD.msg);
-
-		if (GD.order == "getValue")
-		{
-			ValueInput.text = GD.value;
-		}
-	}
+        if (GD.order == "getValue")
+        {
+            
+        }
+    }
 }
