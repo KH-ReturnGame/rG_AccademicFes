6c92e9e4dc14440cce5063cb267c67e3f7e2c4b3
DNSK <sslgh1024@naver.com>
2025-04-04 10:33:53 +0900

    똥 동시 생성

--- Assets/#1 Scripts/CreatePoop.cs
+++ Assets/#1 Scripts/CreatePoop.cs
@@ -8,38 +8,39 @@ public class CreatePoop : MonoBehaviour
     public float spawnInterval = 1.0f; // 장애물 생성 간격
     public float spawnRangeX = 8.0f; // x축 범위 설정
     private float timer = 0.0f; // 시간 누적을 위한 변수
+    private float count = 1;
 
     void Start()
     {
         StartCoroutine(SpawnObstacle());
     }
 
-    void Update()
+    void FixedUpdate()
     {
         timer += Time.deltaTime;
-        if(timer > 15f)
+        if(timer > 30f)
         {
-            StartCoroutine(SpawnObstacle());
+            count++;
             timer = 0;
         }
     }
 
     IEnumerator SpawnObstacle()
     {
-        // 장애물이 생성될 랜덤 x 위치 설정
-        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
-        Vector3 spawnPosition = new Vector3(randomX, transform.position.y, 0);
-
-        float randomRotationZ = Random.Range(0f, 360f); // Z축을 기준으로 회전
-        Quaternion randomRotation = Quaternion.Euler(0, 0, randomRotationZ);
-        
         // 장애물 생성
-        Instantiate(Poop, spawnPosition, randomRotation);
+        for (int i = 0; i < count; i++)
+        {
+            // 장애물이 생성될 랜덤 x 위치 설정
+            float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+            Vector3 spawnPosition = new Vector3(randomX, transform.position.y, 0);
 
+            float randomRotationZ = Random.Range(0f, 360f); // Z축을 기준으로 회전
+            Quaternion randomRotation = Quaternion.Euler(0, 0, randomRotationZ);
+            
+            Instantiate(Poop, spawnPosition, randomRotation);
+        }
         yield return new WaitForSeconds(spawnInterval);
-
         StartCoroutine(SpawnObstacle());
-
         yield return null;
     }
 
