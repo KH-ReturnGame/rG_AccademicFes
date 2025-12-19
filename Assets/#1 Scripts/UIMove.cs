556b40cf03b9220f926cfc8ea15ec006cbdb3730
이형주 <jujumom630@gmail.com>
2025-12-20 03:53:53 +0900

    이걸로도 좋을지 일단 작동은 되는 거 같은데

--- Assets/#1 Scripts/UIMove.cs
+++ Assets/#1 Scripts/UIMove.cs
@@ -6,6 +6,7 @@ public class UIMove : MonoBehaviour
 {
     public GameObject playerObj;
     public Player player;
+    float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,24 @@ public class UIMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = new Vector3(horizontalInput, 0, 0) * 7.5f * Time.deltaTime;
         
+        // 이동 적용
+        playerObj.transform.Translate(movement);
+
+        // 화면 밖으로 나가지 않도록 제한
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -8.0f, 8.0f); // x 좌표 제한 (화면 경계 설정)
+        transform.position = clampedPosition;
     }
 
-    public void UIMoving()
+    public void UIMovingLeft()
     {
-        
+        horizontalInput = -1;
+    }
+
+    public void UIMovingRight()
+    {
+        horizontalInput = 1;
     }
 }
